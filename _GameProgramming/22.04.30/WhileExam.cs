40885882ab0cdb99cb84f8f705e7acca3349adfe
정윤혁 <yunhyukjeong02@gmail.com>
2022-04-30 11:23:17 +0900

    220430 GameProgramming

---
+++ _GameProgramming/22.04.30/WhileExam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class WhileExam
+{
+    static void Main()
+    {
+        int number = 3;
+
+        while (number <= 9)
+        {
+            Console.WriteLine(number);
+            number = number + 3;
+        }
+    }
+}
---
+++ _GameProgramming/22.04.30/WhilePractice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class WhilePractice
+{
+    static void Main()
+    {
+        int number = 0;
+
+        while (number <= 4)
+        {
+            Console.WriteLine(number);
+            number = number + 2;
+        }
+    }
+}
